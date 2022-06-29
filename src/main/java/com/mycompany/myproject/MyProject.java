27867ddb19258189a35e09ab74989b63f6efb7fd
amypa <amypa@10.111.72.101>
2022-06-29 14:35:14 -0400

    Amy Version 1.2

--- src/main/java/com/mycompany/myproject/MyProject.java
+++ src/main/java/com/mycompany/myproject/MyProject.java
@@ -13,5 +13,6 @@ public class MyProject {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("This code is altred by Amy");
     }
 }
